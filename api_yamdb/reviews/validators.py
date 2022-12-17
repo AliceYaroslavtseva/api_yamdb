2ab8115eb0bb28fd5531e2af985fdd46c5d5d083
Nikolay <110336434+kichannf@users.noreply.github.com>
2022-12-17 12:19:08 +0400

    Update validators.py

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -6,8 +6,3 @@ def validate_year(value):
     """Проверка корректности даты фильма"""
     if value > datetime.now().year:
         raise ValidationError('Увы, будущее еще не наступило')
-    if value < 1895:
-        raise ValidationError(
-            f'Кажется, в {value} году еще не было фильмов. '
-            f'Первый фильм был снят в 1895 году.'
-        )
